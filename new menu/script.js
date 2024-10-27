685ef2db5a270a34218b78c84fa32f12c1a1d6a2
Yun-Woo Chung <jsjsone0813@gmail.com>
2024-10-26 23:42:07 -0400

    Completed Navigation bar, updated header

--- new menu/script.js
+++ new menu/script.js
@@ -1,16 +1,27 @@
+
+/* Function to give hamburger functionality and animation */
 function animateMenu(x) {
     x.classList.toggle("change");
   }
 
-let navMenu = document.getElementById("nav-menu");
-let navButton = document.getElementById("sidebar");
-let navOpen = false;
+/* Function to display sidebar when clicked */
+const navMenu = document.getElementById("overlay");
+const navButton = document.getElementById("hamburger");
+const barOne = document.getElementById("bar1");
+const barTwo = document.getElementById("bar2");
+const barThree = document.getElementById("bar3");
+let overlayOpen = false;
 navButton.addEventListener("click" , function() {
-  if (navOpen == false) {
-    navMenu.classList.add("open");
-    navOpen = true;
-  } else if (navOpen == true) {
-    navMenu.classList.remove("open");
-    navOpen = false;
+  overlayOpen = !overlayOpen;
+  if (overlayOpen) {
+    navMenu.style.width = "100%";
+    barOne.style.backgroundColor = "white";
+    barTwo.style.backgroundColor="white";
+    barThree.style.backgroundColor="white";
+  } else {
+    navMenu.style.width = "0%";
+    barOne.style.backgroundColor="#333";
+    barTwo.style.backgroundColor="#333";
+    barThree.style.backgroundColor="#333";
   }
 });
