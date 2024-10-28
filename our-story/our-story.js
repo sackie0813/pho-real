23bdd581d1e9a682ae002e882a61403625d51de7
sackie0813 <jsjsone0813@gmail.com>
2024-10-28 11:51:05 -0400

    Updated about us page, added functionality including a  footer & image gallery

--- our-story/our-story.js
+++ our-story/our-story.js
@@ -28,3 +28,36 @@ navButton.addEventListener("click" , function() {
     barThree.style.backgroundColor="#333";
   }
 });
+
+
+/* Function to increment and change slides in clickable slide window*/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Function to increase incrememnt slide #*/
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display="block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
