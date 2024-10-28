e16428dcc9d2d23df89eef46aa5d9315d46c1546
sackie0813 <jsjsone0813@gmail.com>
2024-10-27 21:33:29 -0400

    Created basic skeleton code for story, changed some aesthetics

---
+++ our-story/our-story.js
@@ -0,0 +1,30 @@
+
+/* Function to give hamburger functionality and animation */
+function animateMenu(x) {
+    x.classList.toggle("change");
+  }
+
+/* Function to display sidebar when clicked */
+const navMenu = document.getElementById("overlay");
+const navButton = document.getElementById("hamburger");
+const barOne = document.getElementById("bar1");
+const barTwo = document.getElementById("bar2");
+const barThree = document.getElementById("bar3");
+let overlayOpen = false;
+navButton.addEventListener("click" , function() {
+  overlayOpen = !overlayOpen;
+  if (overlayOpen) {
+    navMenu.style.width = "100%";
+    navMenu.style.overflowX="hidden";
+    document.body.style.overflow = "hidden";
+    barOne.style.backgroundColor = "white";
+    barTwo.style.backgroundColor="white";
+    barThree.style.backgroundColor="white";
+  } else {
+    navMenu.style.width = "0%";
+    navMenu.style.overflowX="";
+    barOne.style.backgroundColor="#333";
+    barTwo.style.backgroundColor="#333";
+    barThree.style.backgroundColor="#333";
+  }
+});
